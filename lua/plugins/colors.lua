36fd756f049ecb455a19b033c7e74ccf28dd23b0
YasserAO <aouffenyasser@gmail.com>
2026-08-22 21:45:20 +0000

    feat(theme): enhance tokyonight, lualine, bufferline, and UI styling

--- lua/plugins/colors.lua
+++ lua/plugins/colors.lua
@@ -1,26 +1,132 @@
-local function enable_transparency()
-    vim.api.nvim_set_hl(0, "Normal", {bg = "none"})
-end
-
 return {
   {
     "folke/tokyonight.nvim",
     lazy = false,
     priority = 1000,
-    config = function()
+    opts = {
+      style = "night", -- "night", "storm", "moon", or "day"
+      transparent = true,
+      styles = {
+        sidebars = "transparent",
+        floats = "transparent",
+        comments = { italic = true },
+        keywords = { italic = true },
+        functions = {},
+        variables = {},
+      },
+      dim_inactive = false,
+      lualine_bold = true,
+      on_highlights = function(hl, c)
+        hl.LineNr = { fg = c.dark5 }
+        hl.CursorLineNr = { fg = c.orange, bold = true }
+        hl.FloatBorder = { fg = c.blue0, bg = "none" }
+        hl.NormalFloat = { bg = "none" }
+        hl.TelescopeBorder = { fg = c.blue0, bg = "none" }
+        hl.TelescopePromptBorder = { fg = c.magenta, bg = "none" }
+        hl.TelescopePromptTitle = { fg = c.bg, bg = c.magenta, bold = true }
+        hl.TelescopePreviewTitle = { fg = c.bg, bg = c.green, bold = true }
+        hl.TelescopeResultsTitle = { fg = c.bg, bg = c.blue, bold = true }
+      end,
+    },
+    config = function(_, opts)
+      require("tokyonight").setup(opts)
       vim.cmd.colorscheme("tokyonight")
-      enable_transparency()
     end,
   },
   {
     "nvim-lualine/lualine.nvim",
-    dependencies = {
-	"nvim-tree/nvim-web-devicons",
-	},
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    event = "VeryLazy",
     opts = {
-	theme = 'tokyonight',
-
+      options = {
+        theme = "tokyonight",
+        globalstatus = true,
+        component_separators = { left = "│", right = "│" },
+        section_separators = { left = "", right = "" },
+        disabled_filetypes = {
+          statusline = { "help", "lazy" },
+        },
+      },
+      sections = {
+        lualine_a = {
+          { "mode", icon = "" },
+        },
+        lualine_b = {
+          { "branch", icon = "" },
+          {
+            "diff",
+            symbols = { added = " ", modified = " ", removed = " " },
+          },
+        },
+        lualine_c = {
+          {
+            "filename",
+            path = 1, -- Relative path
+            symbols = { modified = " ●", readonly = " ", unnamed = "[No Name]" },
+          },
+        },
+        lualine_x = {
+          {
+            "diagnostics",
+            sources = { "nvim_diagnostic" },
+            symbols = { error = " ", warn = " ", info = " ", hint = "󰌵 " },
+          },
+          { "filetype", icon_only = false },
+        },
+        lualine_y = { "progress" },
+        lualine_z = {
+          { "location", icon = "" },
+        },
+      },
     },
-
-  }, 
+  },
+  {
+    "akinsho/bufferline.nvim",
+    version = "*",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    event = "VeryLazy",
+    opts = {
+      options = {
+        mode = "buffers",
+        separator_style = "slant",
+        always_show_bufferline = false,
+        show_buffer_close_icons = false,
+        show_close_icon = false,
+        diagnostics = "nvim_lsp",
+        offsets = {
+          {
+            filetype = "NvimTree",
+            text = "File Explorer",
+            highlight = "Directory",
+            text_align = "left",
+          },
+        },
+      },
+    },
+  },
+  {
+    "lukas-reineke/indent-blankline.nvim",
+    main = "ibl",
+    event = { "BufReadPre", "BufNewFile" },
+    opts = {
+      indent = {
+        char = "│",
+        tab_char = "│",
+      },
+      scope = {
+        enabled = true,
+        show_start = false,
+        show_end = false,
+        highlight = { "Function", "Label" },
+      },
+      exclude = {
+        filetypes = { "help", "lazy", "mason", "notify" },
+      },
+    },
+  },
+  {
+    "stevearc/dressing.nvim",
+    event = "VeryLazy",
+    opts = {},
+  },
 }
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,16 +1,47 @@
-return
-{
-    'nvim-telescope/telescope.nvim', version = '*',
+return {
+  {
+    "nvim-telescope/telescope.nvim",
+    version = "*",
     dependencies = {
-	'nvim-lua/plenary.nvim',
-	{ 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+      "nvim-lua/plenary.nvim",
+      { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
     },
-    config = function()
-	local builtin = require('telescope.builtin')
-	vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
-	vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
-	vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
-	vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
-    end
+    opts = {
+      defaults = {
+        prompt_prefix = "   ",
+        selection_caret = " ❯ ",
+        entry_prefix = "   ",
+        layout_strategy = "horizontal",
+        layout_config = {
+          horizontal = {
+            prompt_position = "top",
+            preview_width = 0.55,
+            results_width = 0.8,
+          },
+          vertical = {
+            mirror = false,
+          },
+          width = 0.87,
+          height = 0.80,
+          preview_cutoff = 120,
+        },
+        sorting_strategy = "ascending",
+        winblend = 0,
+        border = true,
+        borderchars = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" },
+      },
+    },
+    config = function(_, opts)
+      local telescope = require("telescope")
+      telescope.setup(opts)
+      pcall(telescope.load_extension, "fzf")
 
+      local builtin = require("telescope.builtin")
+      vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope: Find files" })
+      vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope: Live grep" })
+      vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope: Buffers" })
+      vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Telescope: Help tags" })
+      vim.keymap.set("n", "<leader>fr", builtin.oldfiles, { desc = "Telescope: Recent files" })
+    end,
+  },
 }
